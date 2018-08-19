ce7fde2be2244170f02076b1e8e9b61bebb9bbf9
bummoKoo <karismakbm@naver.com>
2018-08-19 22:46:10 +0900

    Add logos

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App.vue'
 import router from './router/index.js'
+import Vuetify from 'vuetify'
 
+Vue.use(Vuetify)
 Vue.use(VueRouter)
 
 new Vue({
